290c165e33f601199e4e1ce7a39fb69c57713aa1
Sebastien Dubois <sdubois.sebastien@gmail.com>
2026-02-17 11:09:50 -0500

    add tokenizer

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
-import { loadDocuments } from './src/data';
+import { buildTokenizer, loadDocuments } from './src/data';
 
 console.log("microgpt-ts");
 
 const docs = await loadDocuments();
+const tokenizer = buildTokenizer(docs);
+
+console.log(`vocab size: ${tokenizer.vocabSize}`);
+console.log(`chars: ${tokenizer.chars.join('')}`);
