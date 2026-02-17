885781667779a95f9c11733a3c84d0eb473cbdf6
Sebastien Dubois <sdubois.sebastien@gmail.com>
2026-02-17 12:31:28 -0500

    add randomGaussian

--- src/utils.ts
+++ src/utils.ts
@@ -16,6 +16,16 @@ export function sample(weights: number[]): number {
   return weights.length - 1;
 }
 
+// Gaussian random number using Box-Muller transform
+// https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+export function randomGaussian(mean = 0, std = 1): number {
+  let u1 = 0;
+  while (u1 === 0) u1 = Math.random(); // avoid 0
+  const u2 = Math.random();
+  const z = Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2);
+  return mean + std * z;
+}
+
 // Fisher-Yates shuffle (in-place)
 export function shuffle<T>(arr: T[]): T[] {
   for (let i = arr.length - 1; i > 0; i--) {
